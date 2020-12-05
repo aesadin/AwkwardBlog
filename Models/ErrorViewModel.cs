d05ae9e3c6106653b053e76efe9e98a65ca78b3f
Allison Sadin <aesadin@gmail.com>
2020-12-04 18:30:15 -0800

    create directory, add background image and about text

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AwkwardBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
